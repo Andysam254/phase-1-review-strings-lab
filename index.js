11c2eb84eb65a13036547b50961ef8c198d8e316
andysam <andygitau444@gmail.com>
2024-09-26 11:54:06 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'samuel';
+// const welcomeMessage = 'Welcome to Flatbook, '
+// const welcomeMessage = 'Welcome to Flatbook, samuel';
+const welcomeMessage = 'Welcome to Flatbook, samuel!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// const shortGreeting = 'Welcome, ';
+// const shortGreeting = 'Welcome, samuel!';
+const shortGreeting = 'Welcome, s!';
